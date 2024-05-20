93aad835abe99e72347c4f3cf8ae2dd78e8d0bf4
seven1106 <that9011@gmail.com>
2024-05-20 16:26:54 +0700

    Search Product API

--- controllers/product.js
+++ controllers/product.js
@@ -10,14 +10,14 @@ exports.fetchProductsCategory = async (req, res) => {
     res.status(500).json({ error: e.message });
   }
 };
-// exports.searchProducts = async (req, res) => {
-//   try {
-//     const products = await Product.find({
-//       name: { $regex: req.params.name, $options: "i" },
-//     });
+exports.searchProducts = async (req, res) => {
+  try {
+    const products = await Product.find({
+      name: { $regex: req.params.name, $options: "i" },
+    });
 
-//     res.json(products);
-//   } catch (e) {
-//     res.status(500).json({ error: e.message });
-//   }
-// };
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
--- routes/product.js
+++ routes/product.js
@@ -5,18 +5,6 @@ const router = express.Router();
 
 router.get("/products/", auth, controller.fetchProductsCategory);
 
-// create a get request to search products and get them
-// /api/products/search/i
-// router.get("/api/products/search/:name", auth, async (req, res) => {
-//   try {
-//     const products = await Product.find({
-//       name: { $regex: req.params.name, $options: "i" },
-//     });
-
-//     res.json(products);
-//   } catch (e) {
-//     res.status(500).json({ error: e.message });
-//   }
-// });
+router.get("/products/search/:name", controller.searchProducts);
 
 module.exports = router;
