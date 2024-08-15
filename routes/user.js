70f1bb56ff57839c86f054fd066c90ed65c23bd3
seven1106 <ntt601121@gmail.com>
2024-08-15 11:15:44 +0700

    place order

--- routes/user.js
+++ routes/user.js
@@ -72,42 +72,42 @@ router.post("/user/save-user-address", auth, async (req, res) => {
   }
 });
 
-// // order product
-// router.post("/user/order", auth, async (req, res) => {
-//   try {
-//     const { cart, totalPrice, address } = req.body;
-//     let products = [];
+// order product
+router.post("/user/order", auth, async (req, res) => {
+  try {
+    const { cart, totalPrice, address } = req.body;
+    let products = [];
 
-//     for (let i = 0; i < cart.length; i++) {
-//       let product = await Product.findById(cart[i].product._id);
-//       if (product.quantity >= cart[i].quantity) {
-//         product.quantity -= cart[i].quantity;
-//         products.push({ product, quantity: cart[i].quantity });
-//         await product.save();
-//       } else {
-//         return res
-//           .status(400)
-//           .json({ msg: `${product.name} is out of stock!` });
-//       }
-//     }
+    for (let i = 0; i < cart.length; i++) {
+      let product = await Product.findById(cart[i].product._id);
+      if (product.quantity >= cart[i].quantity) {
+        product.quantity -= cart[i].quantity;
+        products.push({ product, quantity: cart[i].quantity });
+        await product.save();
+      } else {
+        return res
+          .status(400)
+          .json({ msg: `${product.name} is out of stock!` });
+      }
+    }
 
-//     let user = await User.findById(req.user);
-//     user.cart = [];
-//     user = await user.save();
+    let user = await User.findById(req.user);
+    user.cart = [];
+    user = await user.save();
 
-//     let order = new Order({
-//       products,
-//       totalPrice,
-//       address,
-//       userId: req.user,
-//       orderedAt: new Date().getTime(),
-//     });
-//     order = await order.save();
-//     res.json(order);
-//   } catch (e) {
-//     res.status(500).json({ error: e.message });
-//   }
-// });
+    let order = new Order({
+      products,
+      totalPrice,
+      address,
+      userId: req.user,
+      orderedAt: new Date().getTime(),
+    });
+    order = await order.save();
+    res.json(order);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 // router.get("/user/orders/me", auth, async (req, res) => {
 //   try {
