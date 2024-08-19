2d11bdfc30ccea124fdba32de2684ef53c17f826
seven1106 <ntt601121@gmail.com>
2024-08-19 11:29:43 +0700

    vendor - Viewing All Orders

--- controllers/vendor.js
+++ controllers/vendor.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { Product } = require("../models/product");
+const Order = require("../models/order");
 const { PromiseProvider } = require("mongoose");
 
 exports.addProduct = async (req, res) => {
@@ -27,11 +28,30 @@ exports.getProducts = async (req, res) => {
     res.status(500).json({ error: e.message });
   }
 }
+exports.getOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({});
+    res.json(orders);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}
 exports.deleteProduct = async (req, res) => {
   try {
     const { id } = req.body;
     let product = await Product.findByIdAndDelete(id);
     res.json(product);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+exports.changeOrderStatus = async (req, res) => {
+  try {
+    const { id, status } = req.body;
+    let order = await Order.findById(id);
+    order.status = status;
+    order = await order.save();
+    res.json(order);
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
